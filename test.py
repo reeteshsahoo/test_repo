c0c0db18f382fb781cb7916f5c1a5db05950a5b7
Reetesh Sahoo <reetesh.sahoo@gmail.com>
2025-06-08 10:40:52 +0530

    commit#1 with muliply method

---
+++ test.py
@@ -0,0 +1,9 @@
+# write a mutliplication function
+def multiply(a, b):
+    return a * b
+
+
+main = __name__ == "__main__"
+if main:
+    # Test the multiplication function
+    print("Multiplication of 3 and 4:", multiply(3, 4))  # Expected output: 12
