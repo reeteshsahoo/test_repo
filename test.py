100d03292064520819bca35a9ba6b2e78b9213bd
Reetesh Sahoo <reetesh.sahoo@gmail.com>
2025-06-08 10:44:06 +0530

    commit2 with addition method

--- test.py
+++ test.py
@@ -3,7 +3,15 @@ def multiply(a, b):
     return a * b
 
 
+# write a addition function
+def add(a, b):
+    return a + b
+
+
 main = __name__ == "__main__"
 if main:
     # Test the multiplication function
     print("Multiplication of 3 and 4:", multiply(3, 4))  # Expected output: 12
+
+    # Test the addition function
+    print("Addition of 3 and 4:", add(3, 4))  # Expected output: 7
